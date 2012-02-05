182c239e0769ccd87253c63b65ca667109af2749
k4s4s.heavener <k4s4s.heavener@gmail.com>
2012-02-05 15:30:01 +0000

    added libxml and libcurl, probably need to be recompiled on each machine

---
+++ libxml/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/kasas/CSD/cs557/csrfwebanalyser/libxml/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/home/kasas/CSD/cs557/csrfwebanalyser/libxml/include/libxml2"
+MODULE_VERSION="xml2-2.7.4"
+
