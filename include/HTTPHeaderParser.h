f2f772ccb9969a1c93ee5b7da298e79ed3fb8719
dimstamat <dimstamat@gmail.com>
2012-02-06 00:38:18 +0000

    nea

--- include/HTTPHeaderParser.h
+++ include/HTTPHeaderParser.h
@@ -2,7 +2,16 @@
 #ifndef __CRAWLER_H__
 #define __CRAWLER_H__
 
+
+#include <list>
+#include "includes.h"
+
+using namespace std;
+
+#define DEBUG_LEVEL 1
+
+
 /* parses headers and tries to identify CSRF defence */
-void check_for_headers(char* header, const char* url);
+void check_for_headers(char* header, const char* url, list< pair < CSRF_Defenses, string > > * results);
 
 #endif
