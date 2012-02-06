035e771d9e96c1b67812f171e3c7d6bc703e406a
dimstamat@gmail.com <dimstamat@gmail.com@94f2a34a-0094-2f99-4db9-88c8903e8028>
2012-02-06 00:38:18 +0000

    nea

--- trunk/src/HTTPHeaderParser.cc
+++ trunk/src/HTTPHeaderParser.cc
@@ -4,44 +4,75 @@
 
 using namespace std;
 
-void check_for_headers(char* header, const char* url){
+#include "includes.h"
+
+
+
+
+void check_for_headers(char* header, const char* url, list< pair < CSRF_Defenses, string > > * results){
 	/*  CSRF defense headers */
 	/***********************************************
 	Custom HTTP Header
 	X-Requested-By: XMLHttpRequest
 	------------------------------------------------
-	X-Frame-Options: SAMEORIGIN
+	X-Frame-Options: SAMEORIGIN, DENY, etc.
 	------------------------------------------------
-	Access-Control-Allow-Origin Header: *
+	Access-Control-Allow-Origin Header: *, smth
 	------------------------------------------------
 	************************************************/
 	int location,i;
 	if((location=string(header).find(string("X-Requested-By")))!=string::npos){
-		fprintf(stderr, "---------------\n%s\n", url);
+		#if DEBUG_LEVEL > 0
+			fprintf(stderr, "---------------\n%s\n", url);
+		#endif
 		i=location;
+		string firstline="";
 		while(header[i]!='\n'){
-			fprintf(stderr, "%c", header[i]);
+			#if DEBUG_LEVEL > 0
+				fprintf(stderr, "%c", header[i]);
+			#endif
+			firstline+=header[i];
 			i++;
 		}
-		fprintf(stderr, "\n");
+		#if DEBUG_LEVEL > 0
+			fprintf(stderr, "\n");
+		#endif
+		results->push_back(make_pair(CUSTOM_HTTP_HEADER, firstline.substr(15) ));
 	}
 	if((location=string(header).find(string("X-Frame-Options")))!=string::npos){
-		fprintf(stderr, "---------------\n%s\n", url);
+		#if DEBUG_LEVEL > 0
+			fprintf(stderr, "---------------\n%s\n", url);
+		#endif
 		i=location;
+		string firstline="";
 		while(header[i]!='\n'){
-			fprintf(stderr, "%c", header[i]);
+			#if DEBUG_LEVEL > 0
+				fprintf(stderr, "%c", header[i]);
+			#endif
+			firstline+=header[i];
 			i++;
 		}
-		fprintf(stderr, "\n");
+		#if DEBUG_LEVEL > 0
+			fprintf(stderr, "\n");
+		#endif
+		results->push_back(make_pair(X_FRAME_OPTIONS_HEADER, firstline.substr(16)));
 	}
 	if((location=string(header).find(string("Access-Control-Allow-Origin")))!=string::npos){
-		fprintf(stderr, "---------------\n%s\n", url);
+		#if DEBUG_LEVEL > 0
+			fprintf(stderr, "---------------\n%s\n", url);
+		#endif
 		i=location;
+		string firstline="";
 		while(header[i]!='\n'){
-			fprintf(stderr, "%c", header[i]);
+			#if DEBUG_LEVEL > 0
+				fprintf(stderr, "%c", header[i]);
+			#endif
+			firstline+=header[i];
 			i++;
 		}
-		fprintf(stderr, "\n");
+		#if DEBUG_LEVEL > 0
+			fprintf(stderr, "\n");
+		#endif
+		results->push_back(make_pair(ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, firstline.substr(28)));
 	}
-
 }
