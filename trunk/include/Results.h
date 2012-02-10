5a62699895ac429b6993b1f98f1c3539edf2dc41
dimstamat@gmail.com <dimstamat@gmail.com@94f2a34a-0094-2f99-4db9-88c8903e8028>
2012-02-10 04:41:37 +0000

    option for results with web site - defenses, and not only URL - defenses

--- trunk/include/Results.h
+++ trunk/include/Results.h
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+#define REPORT_DISTINCT_URLS 0
+
+#if REPORT_DISTINCT_URLS
 class Results {
 	private:
 	map<string, list<pair<string, string> > > urlDefensesMap;
@@ -22,5 +25,24 @@ class Results {
 	void PrintUrlsMap(void);
 	void PrintDefensesMap(void);
 };
+
+#else
+class Results {
+	private:
+	map<string, map<string, string> > urlDefensesMap;
+	map<string, map<string, string> > defenseUrlsMap;
+	public:
+	Results();
+	map<string, map<string, string> > GetUrlDefensesMap(void);
+	map<string, map<string, string> > GetDefenseUrlsMap(void);
+	void AddUrlDefense(string url, const string policy, const string value);
+	void AddDefenseUrl(string policy, string url, const string value);
+	void MergeUrlDefensesMaps(map<string, map<string, string> > mapA);
+	void MergeDefenseUrlsMaps(map<string, map<string, string> > mapA);
+	void PrintUrlsMap(void);
+	void PrintDefensesMap(void);
+};
+
+#endif
 
 #endif
